c54569740a65df72a18741dc98752d455d3c8eb2
jochongs <joch2712@naver.com>
2024-04-15 02:50:28 +0900

    feat: Create service instance

---
+++ src/api/index.service.js
@@ -0,0 +1,21 @@
+const HashService = require('../common/hash.service');
+const JwtService = require('../common/jwt.service');
+const AuthService = require('./auth/auth.service');
+const { todoRepository, userRepository } = require('./index.repository');
+const TodoService = require('./todo/todo.service');
+const UserService = require('./user/user.service');
+
+const hashService = new HashService();
+const jwtService = new JwtService();
+
+const todoService = new TodoService(todoRepository);
+const userService = new UserService(userRepository, hashService);
+const authService = new AuthService(userRepository, jwtService, userService, hashService);
+
+module.exports = {
+    hashService,
+    jwtService,
+    todoService,
+    userService,
+    authService,
+};
