477ecb16b50b1f4b5f6862367d762e35564bdab7
jochongs <joch2712@naver.com>
2024-04-15 00:52:21 +0900

    feat: Create Todo model

---
+++ src/api/todo/model/todo.model.js
@@ -0,0 +1,31 @@
+module.exports = class ToDo {
+    /**
+     * @type {number}
+     */
+    idx;
+
+    /**
+     * @type {number}
+     */
+    userIdx;
+
+    /**
+     * @type {string}
+     */
+    userNickname;
+
+    /**
+     * @type {string}
+     */
+    title;
+
+    /**
+     * @type {string}
+     */
+    contents;
+
+    /**
+     * @type {Date}
+     */
+    createdAt;
+};
