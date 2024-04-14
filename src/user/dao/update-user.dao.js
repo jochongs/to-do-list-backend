e599093f406211facc52aec60fa7a95fedac7fc4
jochongs <joch2712@naver.com>
2024-04-14 23:36:13 +0900

    feat: Create UpdateUserDao

---
+++ src/user/dao/update-user.dao.js
@@ -0,0 +1,16 @@
+module.exports = class UpdateUserDao {
+    /**
+     * @type {string}
+     */
+    nickname;
+
+    /**
+     * @type {string}
+     */
+    id;
+
+    /**
+     * @type {string}
+     */
+    pw;
+};
