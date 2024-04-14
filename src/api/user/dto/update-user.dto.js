1fe0e9c5b5d9e7632dd1c1bfedb5e1e354af6db1
jochongs <joch2712@naver.com>
2024-04-15 00:35:11 +0900

    feat: Create UpdateUserDto

---
+++ src/api/user/dto/update-user.dto.js
@@ -0,0 +1,32 @@
+const { BadRequestException } = require('../../../exception/BadRequestException');
+
+module.exports = class UpdateUserDto {
+    /**
+     * @type {string}
+     */
+    nickname;
+
+    /**
+     * @param {{
+     *  nickname: string
+     * }} data
+     */
+    constructor(data) {
+        this.nickname = data.nickname;
+    }
+
+    /**
+     * @param {{
+     *  nickname: string
+     * }} data
+     */
+    static createUpdateDto(data) {
+        if (!nickname) {
+            throw new BadRequestException('Invalid nickname');
+        }
+
+        return new UpdateUserDto({
+            nickname: data.nickname,
+        });
+    }
+};
