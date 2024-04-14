525c4cb836df2fb3fb005aee5666f6dbfab319a3
jochongs <joch2712@naver.com>
2024-04-15 02:28:38 +0900

    feat: Create updateTodo

--- src/api/todo/todo.controller.js
+++ src/api/todo/todo.controller.js
@@ -1,5 +1,7 @@
 const LoginUserEntity = require('../auth/entity/LoginUserEntity');
 const CreateTodoDto = require('./dto/create-todo.dto');
+const UpdateTodoDto = require('./dto/update-todo.dto');
+const PermissionDeniedException = require('./exception/PermissionDeniedException');
 const TodoService = require('./todo.service');
 
 module.exports = class TodoController {
@@ -21,5 +23,22 @@ module.exports = class TodoController {
         res.status(200).send({
             todo,
         });
+    }
+
+    async updateTodo(req, res) {
+        const loginUser = LoginUserEntity.createLoginUserEntity(req.user);
+        const todoIdx = Number(req.params.idx);
+
+        const todoEntity = await this.todoService.getTodoByIdx(todoIdx);
+
+        if (todoEntity.author.idx !== loginUser.idx) {
+            throw new PermissionDeniedException('Permission denied');
+        }
+
+        const updateDto = UpdateTodoDto.createUpdateTodoDto(req.body);
+
+        await this.todoService.updateTodoByIdx(todoIdx, updateDto);
+
+        res.status(201).end();
     }
 };
