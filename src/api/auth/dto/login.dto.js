9111c270a80711d19218a4c265c8fe8d1780ec17
jochongs <joch2712@naver.com>
2024-04-15 00:15:01 +0900

    feat: Create createLoginDto static method

--- src/api/auth/dto/login.dto.js
+++ src/api/auth/dto/login.dto.js
@@ -1,3 +1,5 @@
+const { BadRequestException } = require('../../../exception/BadRequestException');
+
 module.exports = class LoginDto {
     /**
      * @type {string}
@@ -18,5 +20,27 @@ module.exports = class LoginDto {
     constructor(data) {
         this.id = data.idx;
         this.pw = data.pw;
+    }
+
+    /**
+     *
+     * @param {{
+     *  id: string;
+     *  pw: string;
+     * }} data
+     */
+    static createLgoinDto(data) {
+        if (!data.id) {
+            throw new BadRequestException('Invalid id');
+        }
+
+        if (!data.pw) {
+            throw new BadRequestException('Invalid pw');
+        }
+
+        return new LoginDto({
+            id: data.id,
+            pw: data.pw,
+        });
     }
 };
