da487cb7f1731811bc8ceffc42dc3ea8e9395797
jochongs <joch2712@naver.com>
2024-04-15 01:57:34 +0900

    feat: Create getTodoByIdx

--- src/api/todo/todo.service.js
+++ src/api/todo/todo.service.js
@@ -1,5 +1,6 @@
 const CreateTodoDto = require('./dto/create-todo.dto');
 const TodoEntity = require('./entity/todo.entity');
+const TodoNotFoundException = require('./exception/TodoNotFoundException');
 const TodoRepository = require('./todo.repository');
 
 module.exports = class TodoService {
@@ -24,6 +25,21 @@ module.exports = class TodoService {
             title: createDto.title,
             contents: createDto.contents,
         });
+
+        return TodoEntity.createToDoEntity(todo);
+    }
+
+    /**
+     * Get todo by idx
+     *
+     * @param {number} idx
+     */
+    async getTodoByIdx(idx) {
+        const todo = await this.todoRepository.selectByIdx(idx);
+
+        if (!todo) {
+            throw new TodoNotFoundException('Cannot find todo');
+        }
 
         return TodoEntity.createToDoEntity(todo);
     }
