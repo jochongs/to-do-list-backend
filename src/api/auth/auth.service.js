e08bedd24ccbceb1b883008986e4910829b152dc
jochongs <joch2712@naver.com>
2024-04-15 00:28:13 +0900

    feat: Create login method

--- src/api/auth/auth.service.js
+++ src/api/auth/auth.service.js
@@ -1,14 +1,27 @@
+const HashService = require('../../common/hash.service');
+const JwtService = require('../../common/jwt.service');
 const UserRepository = require('../user/user.repository');
+const UserService = require('../user/user.service');
 const LoginDto = require('./dto/login.dto');
+const InvalidIdOrPwException = require('./exception/InvalidIdOrPwException');
 
 module.exports = class AuthService {
     userRepository;
+    userService;
+    jwtService;
+    hashService;
 
     /**
      * @param {UserRepository} userRepository
+     * @param {JwtService} jwtService
+     * @param {UserService} userService
+     * @param {HashService} hashService
      */
-    constructor(userRepository) {
+    constructor(userRepository, jwtService, userService, hashService) {
         this.userRepository = userRepository;
+        this.userService = userService;
+        this.hashService = hashService;
+        this.jwtService = jwtService;
     }
 
     /**
@@ -18,5 +31,26 @@ module.exports = class AuthService {
      */
     async login(loginDto) {
         const user = await this.userRepository.selectUserById(loginDto.id);
+
+        if (!user) {
+            throw new InvalidIdOrPwException('Invalid id');
+        }
+
+        if (this.hashService.compare(loginDto.pw, user.pw)) {
+            throw new InvalidIdOrPwException('Invalid pw');
+        }
+
+        const token = this.jwtService.sign(
+            {
+                idx: user.idx,
+            },
+            {
+                expiresIn: '14d',
+            }
+        );
+
+        return {
+            jwt: token,
+        };
     }
 };
