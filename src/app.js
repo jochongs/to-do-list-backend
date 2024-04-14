00622c84dab52d8e10ccd889bca1c435e730cee7
jochongs <joch2712@naver.com>
2024-04-15 03:11:04 +0900

    feat: Connect router to app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const NotFoundException = require('./common/exception/NotFoundException');
 const Exception = require('./common/exception/Exception');
+const authRouter = require('./api/auth/auth.router');
+const userRouter = require('./api/user/user.router');
+const todoRouter = require('./api/todo/todo.rotuer');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/auth', authRouter);
+app.use('/user', userRouter);
+app.use('/todo', todoRouter);
 
 app.use((req, res, next) => {
     throw new NotFoundException('API not found');
