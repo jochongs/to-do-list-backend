7b11cf30ddf3acfbeb73fa33b744bbc1b150f416
jochongs <joch2712@naver.com>
2024-04-15 02:36:51 +0900

    feat: Create GetTodoDto static method

--- src/api/todo/dto/get-todo.dto.js
+++ src/api/todo/dto/get-todo.dto.js
@@ -8,4 +8,28 @@ module.exports = class GetTodoDto {
      * @type {number | undefined}
      */
     user;
+
+    /**
+     * @param {{
+     *  page: number;
+     *  user: number;
+     * }} data
+     */
+    constructor(data) {
+        this.page = data.page;
+        this.user = data.user;
+    }
+
+    /**
+     * @param {{
+     *  page: string | undefined;
+     *  user: number;
+     * }} data
+     */
+    static createGetTodoDto(data) {
+        return new GetTodoDto({
+            page: data.page ? Number(data.page) : 1,
+            user: data.user ? Number(data.user) : undefined,
+        });
+    }
 };
