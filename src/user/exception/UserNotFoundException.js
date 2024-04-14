3cd79869dff154636ad33a9ef8f904d62a4c4066
jochongs <joch2712@naver.com>
2024-04-14 23:27:17 +0900

    feat: Create UserNotFoundException

---
+++ src/user/exception/UserNotFoundException.js
@@ -0,0 +1,10 @@
+const NotFoundException = require('../../exception/NotFoundException');
+
+module.exports = class UserNotFoundException extends NotFoundException {
+    /**
+     * @param {string} message
+     */
+    constructor(message) {
+        super(message);
+    }
+};
