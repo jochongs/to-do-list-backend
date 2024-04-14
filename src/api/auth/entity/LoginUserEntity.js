906f51ed993cf2ba11fc462b7b75cdf007ed16cc
jochongs <joch2712@naver.com>
2024-04-15 02:18:25 +0900

    feat: Create createLoginUserEntity static method

--- src/api/auth/entity/LoginUserEntity.js
+++ src/api/auth/entity/LoginUserEntity.js
@@ -1,8 +1,24 @@
+const TokenPayloadDto = require('../dto/token-payload.dto');
+
 module.exports = class LoginUserEntity {
     /**
      * @type
      */
     idx;
 
-    static createLoginUserEntity() {}
+    /**
+     * @param {{
+     *  idx: number;
+     * }} data
+     */
+    constructor(data) {
+        this.idx = data.idx;
+    }
+
+    /**
+     * @param {TokenPayloadDto} payload
+     */
+    static createLoginUserEntity(payload) {
+        return new LoginUserEntity(payload.idx);
+    }
 };
