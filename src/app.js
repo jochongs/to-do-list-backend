77a5ec1a6188fe21de2ddff66d924b669b089472
jochongs <joch2712@naver.com>
2024-04-15 03:10:26 +0900

    feat: Create error middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const NotFoundException = require('./common/exception/NotFoundException');
+const Exception = require('./common/exception/Exception');
 
 const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    throw new NotFoundException('API not found');
+});
+
+app.use((err, req, res, next) => {
+    if (err instanceof Exception) {
+        return res.status(err.status).send({
+            message: err.message,
+        });
+    }
+
+    return res.status(500).send({
+        message: '예상하지 못한 에러가 발생했습니다.',
+    });
+});
 
 module.exports = app;
