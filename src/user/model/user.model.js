cfa6b73270037bf903bf665c9c86903c2b2f8653
jochongs <joch2712@naver.com>
2024-04-14 23:19:34 +0900

    feat: Create User model

---
+++ src/user/model/user.model.js
@@ -0,0 +1,26 @@
+class User {
+    /**
+     * @type {number}
+     */
+    idx;
+
+    /**
+     * @type {string}
+     */
+    id;
+
+    /**
+     * @type {string}
+     */
+    pw;
+
+    /**
+     * @type {string}
+     */
+    createdAt;
+
+    /**
+     * @type {string}
+     */
+    deletedAt;
+}
