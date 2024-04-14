290e9d447498b45c749a44848626526fa5cf89e9
jochongs <joch2712@naver.com>
2024-04-14 23:42:04 +0900

    feat: Create UserRepository

---
+++ src/user/user.repository.js
@@ -0,0 +1,114 @@
+const InsertUserDao = require('./dao/insert-user.dao');
+const UpdateUserDao = require('./dao/update-user.dao');
+const UserNotFoundException = require('./exception/UserNotFoundException');
+
+module.exports = class UserRepository {
+    /**
+     * @type {import('pg').Pool}
+     */
+    pool;
+
+    /**
+     *
+     * @param {import('pg').Pool} pool
+     */
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+    /**
+     * Select user by idx
+     * @param {number} idx
+     * @param {import('pg').PoolClient | undefined} conn
+     * @return {Promise<User | null>}
+     */
+    async selectUserByIdx(idx, conn = this.pool) {
+        const queryResult = await conn.query(
+            `SELECT 
+                idx,
+                id,
+                pw,
+                nickname,
+                created_at AS "createdAt",
+                deleted_at AS "deletedAt"
+            FROM
+                user_tb
+            FROM
+                user_tb
+            WHERE
+                idx = $1
+            AND
+                deleted_at IS NULL`,
+            [idx]
+        );
+
+        return queryResult.rows[0] || null;
+    }
+
+    /**
+     * Insert user by idx
+     *
+     * @param {InsertUserDao} insertDao
+     * @param {import('pg').PoolClient | undefined} conn
+     * @param {Promise<User>}
+     */
+    async insertUser(insertDao, conn = this.pool) {
+        const queryResult = await conn.query(
+            `INSERT INTO user_tb 
+                (id, nickname, pw) 
+                    VALUES 
+                ($1, $2, $3)
+            RETURNING
+                idx,
+                id,
+                pw,
+                nickname,
+                created_at AS "createdAt",
+                deleted_at AS "deletedAt"`,
+            [insertDao.id, insertDao.nickname, insertDao.pw]
+        );
+
+        return queryResult.rows[0];
+    }
+
+    /**
+     * Update user by idx
+     *
+     * @param {number} idx
+     * @param {UpdateUserDao} updateDao
+     * @param {import('pg').PoolClient | undefined} conn
+     * @returns {Promise<void>}
+     */
+    async updateUserByIdx(idx, updateDao, conn = this.pool) {
+        await conn.query(
+            `UPDATE 
+                user_tb
+            SET 
+                nickname = $2,
+                id = $3,
+                pw = $4
+            WHERE
+                idx = $1`,
+            [idx, updateDao.nickname, updateDao.id, updateDao.pw]
+        );
+    }
+
+    /**
+     * Delete user by idx
+     * Soft delete
+     *
+     * @param {number} idx
+     * @param {import('pg').PoolClient | undefined} conn
+     * @returns {Promise<void>}
+     */
+    async deleteByUserIdx(idx, conn) {
+        await conn.query(
+            `UPDATE 
+                user_tb 
+            SET 
+                deleted_at = NOW() 
+            WHERE idx = $1`,
+            [idx]
+        );
+    }
+};
