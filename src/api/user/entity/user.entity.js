e5c2fbca5777cc8b5c5a9260050a52ec8dbb123a
jochongs <joch2712@naver.com>
2024-04-15 00:09:09 +0900

    feat: Create UserEntity

---
+++ src/api/user/entity/user.entity.js
@@ -0,0 +1,50 @@
+module.exports = class UserEntity {
+    /**
+     * @type {number}
+     */
+    idx;
+
+    /**
+     * @type {string}
+     */
+    id;
+
+    /**
+     * @type {string}
+     */
+    nickname;
+
+    /**
+     * @type {Date}
+     */
+    createdAt;
+
+    /**
+     * @param {{
+     *  idx: number;
+     *  id: string;
+     *  nickname: string;
+     *  createdAt: Date;
+     * }} data
+     */
+    constructor(data) {
+        this.idx = data.idx;
+        this.id = data.id;
+        this.nickname = data.nickname;
+        this.createdAt = data.createdAt;
+    }
+
+    /**
+     * Create UserEntity with User model
+     *
+     * @param {User} user
+     */
+    static createUserEntity(user) {
+        return new UserEntity({
+            idx: user.idx,
+            id: user.id,
+            nickname: user.nickname,
+            createdAt: new Date(user.createdAt),
+        });
+    }
+};
