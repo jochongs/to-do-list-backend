1d4dc9cdab3c1199cde186a0eb3fa4c9f2c606eb
jochongs <joch2712@naver.com>
2024-04-14 23:34:31 +0900

    feat: Create InsertUserDao

---
+++ src/user/dao/insert-user.dao.js
@@ -0,0 +1,16 @@
+module.exports = class InsertUserDao {
+    /**
+     * @type {string}
+     */
+    nickname;
+
+    /**
+     * @type {string}
+     */
+    id;
+
+    /**
+     * @type {string}
+     */
+    pw;
+};
